e5a35afa3c7956e8e9d33e4284ad19ee825c8563
kirangp <kirangp@gmail.com>
2017-03-26 11:33:14 +1100

    Made changes to show images dynamically

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,7 @@ module EcommerceSite
 
     config.paperclip_defaults = {
       storage: :s3,
+      s3_host_name: "s3-ap-southeast-2.amazonaws.com",
       s3_region: ENV['AWS_REGION'],
       s3_credentials: {
         bucket: ENV['AWS_BUCKET'],
